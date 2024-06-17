e05eafb13f3dca49830897829653f87b6bbb31a3
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-06-17 21:23:39 +0530

    Added `DNSHeader::from_buffer` to create an instance out of a `PacketBuffer`

--- src/dns/dns_header.rs
+++ src/dns/dns_header.rs
@@ -1,3 +1,7 @@
+use crate::utility::packet_buffer::PacketBuffer;
+use crate::utility::result::Result;
+
+#[derive(Clone, Debug)]
 pub struct DNSHeader {
     /**
      * 16-bits
@@ -121,5 +125,47 @@ impl DNSHeader {
             authority_count: 0,
             additional_count: 0,
         }
+    }
+
+    pub fn from_buffer(buffer: &mut PacketBuffer) -> Result<DNSHeader> {
+        let mut result = DNSHeader::new();
+
+        result.id = buffer.read_as_u16()?;
+
+        /*
+        Use the following bit masking to extract the individual bits from the 16-bit flags field:
+        * 0b1000000000000000 = 1 bit  = 0x8000 = Query/Response
+        * 0b0111100000000000 = 4 bits = 0x7800 = Operation code
+        * 0b0000010000000000 = 1 bit  = 0x0400 = Authoritative answer
+        * 0b0000001000000000 = 1 bit  = 0x0200 = Truncated message
+        * 0b0000000100000000 = 1 bit  = 0x0100 = Recursion desired
+        * 0b0000000010000000 = 1 bit  = 0x0080 = Recursion available
+        * 0b0000000001111000 = 3 bits = 0x0078 = Reserved
+        * 0b0000000000001111 = 4 bits = 0x000F = Response code
+        */
+        let flags = buffer.read_as_u16()?;
+        /* 1st bit */
+        result.query_response = (flags & 0b1000000000000000) == 1;
+        /* 2nd to 5th bit = 4 bits */
+        result.op_code = (flags & 0b0111100000000000) as u8;
+        /* 6th bit */
+        result.authoritative_answer = (flags & 0b0000010000000000) == 1;
+        /* 7th bit */
+        result.truncated_message = (flags & 0b0000001000000000) == 1;
+        /* 8th bit */
+        result.recursion_desired = (flags & 0b0000000100000000) == 1;
+        /* 9th bit */
+        result.recursion_available = (flags & 0b0000000010000000) == 1;
+        /* 10th to 12th bit = 3 bits */
+        result.reserved = ((flags & 0b0000000001111000) >> 4) as u8;
+        /* 13th to 16th bit = 4 bits */
+        result.response_code = (flags & 0b0000000000001111) as u8;
+
+        result.question_count = buffer.read_as_u16()?;
+        result.answer_count = buffer.read_as_u16()?;
+        result.authority_count = buffer.read_as_u16()?;
+        result.additional_count = buffer.read_as_u16()?;
+
+        Ok(result)
     }
 }
