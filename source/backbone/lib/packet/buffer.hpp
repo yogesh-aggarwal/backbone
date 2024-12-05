ff02c4e3c2c93d755d79a37623d8157c292a5cdd
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-05 21:49:54 +0530

    Implement PacketBuffer class extending BasicBuffer; add constructors and assignment operators

--- source/backbone/lib/packet/buffer.hpp
+++ source/backbone/lib/packet/buffer.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <backbone/lib/buffer/basic.tcc>
+
+/* ------------------------------------------------------------------------------------------------------- */
+
+class PacketBuffer : public BasicBuffer<uint8_t, 512>
+{
+public:
+   PacketBuffer() : BasicBuffer<uint8_t, 512>(0) {}
+   PacketBuffer(const PacketBuffer &other) : BasicBuffer<uint8_t, 512>(other) {}
+   PacketBuffer(PacketBuffer &&other) : BasicBuffer<uint8_t, 512>(std::move(other)) {}
+   ~PacketBuffer() = default;
+
+   PacketBuffer &
+   operator=(const PacketBuffer &other)
+   {
+      BasicBuffer<uint8_t, 512>::operator=(other);
+      return *this;
+   }
+
+   PacketBuffer &
+   operator=(PacketBuffer &&other)
+   {
+      BasicBuffer<uint8_t, 512>::operator=(std::move(other));
+      return *this;
+   }
+};
+
+/* ------------------------------------------------------------------------------------------------------- */
