c3e3efa0b25a63971d5cc2b75e2c89b8804ce3c8
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-05 21:18:19 +0530

    Refactor CMake configuration and project structure; add run executable

---
+++ cmd/run/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <backbone/lib/buffer/basic_buffer.tcc>
+
+int
+main()
+{
+   BasicBuffer<uint8_t, 10> buf('a');
+
+   // FILL
+   for (int i = 0; i < 3; i++)
+   {
+      auto _ = buf.Write(i + 'a');
+   }
+
+   // READ
+   for (int i = 0; i < 3; i++)
+   {
+      auto result = buf.Read();
+      if (!result) { result.error->Print(); }
+      else { std::cout << "Value: " << result.value << std::endl; }
+   }
+
+   return 0;
+}
--- source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int
-main()
-{
-   std::cout << "Hello DNS" << std::endl;
-
-   return 0;
-}
