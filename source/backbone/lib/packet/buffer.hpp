849182c0800e4ea4b05d97c99420a17913faa85a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-06 22:59:51 +0530

    Update include directives to use .tpp extension for template files

--- source/backbone/lib/packet/buffer.hpp
+++ source/backbone/lib/packet/buffer.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <backbone/lib/buffer/basic.tcc>
+#include <backbone/lib/buffer/basic.tpp>
 
 /* ------------------------------------------------------------------------------------------------------- */
 
