bad3c776e06919d85a2640f85241f33d29801b04
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-06 15:32:57 +0530

    Enhance packet parsing with error handling for answers, authorities, and additionals

--- source/backbone/lib/packet/packet.cpp
+++ source/backbone/lib/packet/packet.cpp
@@ -34,12 +34,46 @@ Packet::from_buffer(Ref<PacketBuffer> buffer) {
    }
 
    /* Parse answers */
+   std::vector<PacketRecord> answers;
+   for (size_t i = 0; i < header.get_value().answer_count; i++) {
+      // Parse answer
+      auto answer = PacketRecord::from_buffer(buffer)
+                        //
+                        .with_catch({ FAILED_TO_PARSE_RECORD, "Failed to parse packet answer" });
+      RETURN_IF_ERROR(answer);
+
+      // Add answer to the list
+      answers.push_back(answer.get_value());
+   }
 
    /* Parse authorities */
+   std::vector<PacketRecord> authorities;
+   for (size_t i = 0; i < header.get_value().authority_count; i++) {
+      // Parse authority
+      auto authority = PacketRecord::from_buffer(buffer)
+                           //
+                           .with_catch({ FAILED_TO_PARSE_RECORD, "Failed to parse packet authority" });
+      RETURN_IF_ERROR(authority);
+
+      // Add authority to the list
+      authorities.push_back(authority.get_value());
+   }
 
    /* Parse additionals */
+   std::vector<PacketRecord> additionals;
+   for (size_t i = 0; i < header.get_value().additional_count; i++) {
+      // Parse additional
+      auto additional = PacketRecord::from_buffer(buffer)
+                            //
+                            .with_catch({ FAILED_TO_PARSE_RECORD, "Failed to parse packet additional" });
+      RETURN_IF_ERROR(additional);
 
-   return Packet(header.get_value(), {}, {}, {}, {});
+      // Add additional to the list
+      additionals.push_back(additional.get_value());
+   }
+
+   auto packet = Packet(header.get_value(), questions, answers, authorities, additionals);
+   return packet;
 }
 
 /* ------------------------------------------------------------------------------------------------------- */
