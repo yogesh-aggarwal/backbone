cbd7c76869347f5172c6a1e5cbf7f87cb9c2bdde
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-06 14:19:03 +0530

    Add constructors to `PacketQuestion` class and define `QueryType` enum for better type management

--- source/backbone/lib/packet/question.cpp
+++ source/backbone/lib/packet/question.cpp
@@ -2,9 +2,28 @@
 
 /* ------------------------------------------------------------------------------------------------------- */
 
+PacketQuestion::PacketQuestion(const std::string &name, QueryType type, uint16_t class_)
+    : m_Name(name), m_Type(type), m_Class(class_) {}
+
+/* ------------------------------------------------------------------------------------------------------- */
+
+PacketQuestion::PacketQuestion(const PacketQuestion &other)
+    : m_Name(other.m_Name), m_Type(other.m_Type), m_Class(other.m_Class) {}
+
+/* ------------------------------------------------------------------------------------------------------- */
+
+PacketQuestion::PacketQuestion(PacketQuestion &&other)
+    : m_Name(std::move(other.m_Name)), m_Type(other.m_Type), m_Class(other.m_Class) {}
+
+/* ------------------------------------------------------------------------------------------------------- */
+
 PacketQuestion
 PacketQuestion::from_buffer(Ref<PacketBuffer> buffer) {
-   return PacketQuestion();
+   // // Assuming PacketBuffer has methods to read data
+   // std::string name   = buffer->read_string();
+   // QueryType   type   = static_cast<QueryType>(buffer->read_uint16());
+   // uint16_t    class_ = buffer->read_uint16();
+   return PacketQuestion("", UNKNOWN, 0);
 }
 
 /* ------------------------------------------------------------------------------------------------------- */
--- source/backbone/lib/packet/question.hpp
+++ source/backbone/lib/packet/question.hpp
@@ -5,6 +5,34 @@
 /* ------------------------------------------------------------------------------------------------------- */
 
 class PacketQuestion {
+public:
+   enum QueryType {
+      UNKNOWN = UINT16_MAX,
+      A       = 1,
+      AAAA    = 28,
+      NS      = 2,
+      CNAME   = 5,
+      SOA     = 6,
+      PTR     = 12,
+      MX      = 15,
+      TXT     = 16,
+      SRV     = 33,
+      ANY     = 255,
+   };
+
+private:
+   std::string m_Name;
+   QueryType   m_Type;
+   uint16_t    m_Class;
+
+public:
+   PacketQuestion() = delete;
+   PacketQuestion(const std::string &name, QueryType type, uint16_t class_);
+   PacketQuestion(const PacketQuestion &other);
+   PacketQuestion(PacketQuestion &&other);
+
+   ~PacketQuestion() = default;
+
    static PacketQuestion
    from_buffer(Ref<PacketBuffer> buffer);
 };
