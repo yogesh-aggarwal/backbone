116386462ca77ed5bc47a7c58b3321926b475487
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-12-06 12:57:35 +0530

    Refactor PacketHeader constructor and from_buffer method for improved readability and error handling

--- source/backbone/lib/packet/header.cpp
+++ source/backbone/lib/packet/header.cpp
@@ -19,9 +19,7 @@ PacketHeader::PacketHeader(uint16_t   id,
       truncated_message(truncated_message), recursion_desired(recursion_desired),
       recursion_available(recursion_available), reserved(reserved), response_code(response_code),
       question_count(question_count), answer_count(answer_count), authority_count(authority_count),
-      additional_count(additional_count)
-{
-}
+      additional_count(additional_count) {}
 
 /* ------------------------------------------------------------------------------------------------------- */
 
@@ -31,9 +29,7 @@ PacketHeader::PacketHeader(const PacketHeader &other)
       recursion_desired(other.recursion_desired), recursion_available(other.recursion_available),
       reserved(other.reserved), response_code(other.response_code), question_count(other.question_count),
       answer_count(other.answer_count), authority_count(other.authority_count),
-      additional_count(other.additional_count)
-{
-}
+      additional_count(other.additional_count) {}
 
 /* ------------------------------------------------------------------------------------------------------- */
 
@@ -43,9 +39,7 @@ PacketHeader::PacketHeader(PacketHeader &&other) noexcept
       recursion_desired(other.recursion_desired), recursion_available(other.recursion_available),
       reserved(other.reserved), response_code(other.response_code), question_count(other.question_count),
       answer_count(other.answer_count), authority_count(other.authority_count),
-      additional_count(other.additional_count)
-{
-}
+      additional_count(other.additional_count) {}
 
 /* ------------------------------------------------------------------------------------------------------- */
 
@@ -65,43 +59,66 @@ PacketHeader::PacketHeader(PacketHeader &&other) noexcept
  *     0000000000001111 = 4 bits = 0x000F = Response code
  */
 Result<PacketHeader>
-PacketHeader::from_buffer(Ref<PacketBuffer> buf)
-{
+PacketHeader::from_buffer(Ref<PacketBuffer> buf) {
    auto _ = buf->SeekRead(0);
+   if (buf->GetReadRemaining() < 12) {
+      return Error({ OUT_OF_BOUNDS, "Buffer overflowed while reading the header" });
+   }
 
    PacketHeader header =
        PacketHeader(0, false, 0, false, false, false, false, 0, ResultCode::NO_ERROR, 0, 0, 0, 0);
 
-   if (buf->GetReadRemaining() < 12)
-      return { header, new Error({ OUT_OF_BOUNDS, "Buffer overflowed while reading the header" }) };
+   // 16 bits
+   auto _id = buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the ID field from buffer" });
+   RETURN_IF_ERROR(_id);
+   header.id = _id.get_value();
 
-   header.id = buf->ReadUInt16().value;   // 2 bytes
+   // 16 bits
+   auto _flags =
+       buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the _flags field from buffer" });
+   RETURN_IF_ERROR(_flags);
+   int flags = _flags.get_value();
 
-   int flags                   = buf->ReadUInt16().value;   // 2 bytes
-   header.query_response       = (flags & 0b1000000000000000);
-   header.op_code              = (flags & 0b0111100000000000);
-   header.authoritative_answer = (flags & 0b0000010000000000);
-   header.truncated_message    = (flags & 0b0000001000000000);
-   header.recursion_desired    = (flags & 0b0000000100000000);
-   header.recursion_available  = (flags & 0b0000000010000000);
-   header.reserved             = (flags & 0b0000000001110000);
+   header.query_response       = (flags & 0b1000000000000000);   // 1 bit
+   header.op_code              = (flags & 0b0111100000000000);   // 4 bits
+   header.authoritative_answer = (flags & 0b0000010000000000);   // 1 bit
+   header.truncated_message    = (flags & 0b0000001000000000);   // 1 bit
+   header.recursion_desired    = (flags & 0b0000000100000000);   // 1 bit
+   header.recursion_available  = (flags & 0b0000000010000000);   // 1 bit
+   header.reserved             = (flags & 0b0000000001110000);   // 3 bits
    header.response_code        = static_cast<ResultCode>((flags & 0b0000000000001111));
 
-   header.question_count   = buf->ReadUInt16().value;   // 2 bytes
-   header.answer_count     = buf->ReadUInt16().value;   // 2 bytes
-   header.authority_count  = buf->ReadUInt16().value;   // 2 bytes
-   header.additional_count = buf->ReadUInt16().value;   // 2 bytes
+   // 16 bits
+   auto _question_count =
+       buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the question count" });
+   RETURN_IF_ERROR(_question_count);
+   header.question_count = _question_count.get_value();
 
-   return { header, nullptr };
+   // 16 bits
+   auto _answer_count = buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the answer count" });
+   RETURN_IF_ERROR(_answer_count);
+   header.answer_count = _answer_count.get_value();
+
+   // 16 bits
+   auto _authority_count =
+       buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the authority count" });
+   RETURN_IF_ERROR(_authority_count);
+   header.authority_count = _authority_count.get_value();
+
+   // 16 bits
+   auto _additional_count =
+       buf->ReadUInt16().with_catch({ OUT_OF_BOUNDS, "Failed to read the additional count" });
+   RETURN_IF_ERROR(_additional_count);
+   header.additional_count = _additional_count.get_value();
+
+   return header;
 }
 
 /* ------------------------------------------------------------------------------------------------------- */
 
 PacketHeader &
-PacketHeader::operator=(const PacketHeader &other)
-{
-   if (this != &other)
-   {
+PacketHeader::operator=(const PacketHeader &other) {
+   if (this != &other) {
       id                   = other.id;
       query_response       = other.query_response;
       op_code              = other.op_code;
@@ -122,10 +139,8 @@ PacketHeader::operator=(const PacketHeader &other)
 /* ------------------------------------------------------------------------------------------------------- */
 
 PacketHeader &
-PacketHeader::operator=(PacketHeader &&other) noexcept
-{
-   if (this != &other)
-   {
+PacketHeader::operator=(PacketHeader &&other) noexcept {
+   if (this != &other) {
       id                   = other.id;
       query_response       = other.query_response;
       op_code              = other.op_code;
@@ -146,8 +161,7 @@ PacketHeader::operator=(PacketHeader &&other) noexcept
 /* ------------------------------------------------------------------------------------------------------- */
 
 void
-PacketHeader::Print(const std::string &name) const
-{
+PacketHeader::Print(const std::string &name) const {
    /* Print title */
    char title[100];
    sprintf(title, "%s Packet Header", name.c_str());
